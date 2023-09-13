87ca9870ff408ef2538e05c20759813d093aee3b
Tanveer143s <116706588+Tanveer143s@users.noreply.github.com>
2023-09-13 13:54:53 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,22 +1,10 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.5.5"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.72"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = ">= 0.8"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.6"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
+      version = ">= 5.13.1"
     }
   }
 }
